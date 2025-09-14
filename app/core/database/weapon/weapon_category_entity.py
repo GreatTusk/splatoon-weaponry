afca3d8ac87a549d2c862717fd7349af27ae541e
f776 <fern.belmar@duocuc.cl>
2025-09-14 17:36:04 -0300

    feat: autogenerate ids

--- app/core/database/weapon/weapon_category_entity.py
+++ app/core/database/weapon/weapon_category_entity.py
@@ -6,7 +6,7 @@ from . import WeaponEntity
 class WeaponCategoryEntity(SQLModel, table=True):
     __tablename__ = "weapon_category"
 
-    id: int | None = Field(primary_key=True)
+    id: int | None = Field(default=None, primary_key=True)
     name: str
 
     weapons: list["WeaponEntity"] = Relationship(back_populates="category")
--- app/core/database/weapon/weapon_entity.py
+++ app/core/database/weapon/weapon_entity.py
@@ -9,7 +9,7 @@ if TYPE_CHECKING:
 class WeaponEntity(SQLModel, table=True):
     __tablename__ = "weapon"
 
-    id: int | None = Field(primary_key=True)
+    id: int | None = Field(default=None, primary_key=True)
     name: str = Field(unique=True)
     category_id: int = Field(foreign_key="weapon_category.id")
 
